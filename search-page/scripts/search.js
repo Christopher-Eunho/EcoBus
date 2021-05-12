f197df4ab1971a2e80d6af6d8005bd24c9ea901e
CALEB VERMA <halasmish@gmail.com>
2021-05-11 22:33:56 -0700

    implement basic functionality for all search page buttons

--- search-page/scripts/search.js
+++ search-page/scripts/search.js
@@ -1,8 +1,18 @@
 let routeSearchForm = document.getElementById("route-search-form");
+
+let methodSelectionBackButton = document.getElementById("method-selection-back-button");
+let selectTransitOption = document.getElementById("method-selection-transit-option-button");
+let selectDrivingOption = document.getElementById("method-selection-driving-option-button");
+
 let navigateUserProfile = document.getElementById("navigate-user-profile");
 let navigateAboutUs = document.getElementById("navigate-about-us");
 let navigateSearchPage = document.getElementById("navigate-search-page");
-let methodSelectionBackButton = document.getElementById("method-selection-back-button");
+
+let transitDetailsBackButton = document.getElementById("transit-option-back-button");
+let saveTransitJourney = document.getElementById("save-transit-journey");
+
+let drivingDetailsBackButton = document.getElementById("driving-option-back-button");
+let saveDrivingJourney = document.getElementById("save-driving-journey");
 
 function initMap() {
     var options = {
@@ -14,6 +24,7 @@ function initMap() {
 
 };
 
+// Search route process
 // Alert values passed to origin and destination form fields
 function alertRouteOriginDestination() {
     let routeOrigin = document.getElementById("route-origin").value;
@@ -35,6 +46,7 @@ routeSearchForm.addEventListener("submit", function (event) {
     methodSelectionContainer.style["justifyContent"] = "space-around";
 });
 
+// Method selection process
 // Go back to search form from method selection
 methodSelectionBackButton.addEventListener("click", function (event) {
     let methodSelectionContainer = document.getElementById("method-selection-container");
@@ -44,6 +56,63 @@ methodSelectionBackButton.addEventListener("click", function (event) {
     searchFormContainer.style["display"] = "block";
 });
 
+// Select transit route
+selectTransitOption.addEventListener("click", function (event) {
+    let methodSelectionContainer = document.getElementById("method-selection-container");
+    methodSelectionContainer.style["display"] = "none";
+    
+    let transitOptionDetails = document.getElementById("transit-option-details-container");
+    transitOptionDetails.style["display"] = "block";
+});
+
+// Select driving route
+selectDrivingOption.addEventListener("click", function (event) {
+    let methodSelectionContainer = document.getElementById("method-selection-container");
+    methodSelectionContainer.style["display"] = "none";
+    
+    let drivingOptionDetails = document.getElementById("driving-option-details-container");
+    drivingOptionDetails.style["display"] = "block";
+});
+
+// Transit route details display
+// Go back to method selection page
+transitDetailsBackButton.addEventListener("click", function (event) {
+    let transitOptionDetails = document.getElementById("transit-option-details-container");
+    transitOptionDetails.style["display"] = "none";
+
+    let methodSelectionContainer = document.getElementById("method-selection-container");
+    methodSelectionContainer.style["display"] = "flex";
+});
+
+// Save journey
+saveTransitJourney.addEventListener("click", function (event) {
+    let transitOptionDetails = document.getElementById("transit-option-details-container");
+    transitOptionDetails.style["display"] = "none";
+    
+    let transitJourneySavedContainer = document.getElementById("transit-journey-saved-container");
+    transitJourneySavedContainer.style["display"] = "block";
+});
+
+// Driving route details display
+// Go back to method selection page
+drivingDetailsBackButton.addEventListener("click", function (event) {
+    let transitOptionDetails = document.getElementById("driving-option-details-container");
+    transitOptionDetails.style["display"] = "none";
+
+    let methodSelectionContainer = document.getElementById("method-selection-container");
+    methodSelectionContainer.style["display"] = "flex";
+});
+
+// Save journey 
+saveDrivingJourney.addEventListener("click", function (event) {
+    let drivingOptionDetails = document.getElementById("driving-option-details-container");
+    drivingOptionDetails.style["display"] = "none";
+    
+    let drivingJourneySavedContainer = document.getElementById("driving-journey-saved-container");
+    drivingJourneySavedContainer.style["display"] = "block";
+});
+
+// Navbar buttons
 // Alert user in place of navigating to User Profile
 navigateUserProfile.addEventListener("click", function (event){
     alert("Take me to the User Profile!");
