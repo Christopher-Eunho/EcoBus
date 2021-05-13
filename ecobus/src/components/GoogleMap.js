1db96f78dc69e8415949306dd06180801bce158a
Christopher-Eunho <christopher.eunho@gmail.com>
2021-05-13 12:30:02 -0700

    search places auto complete done

--- ecobus/src/components/GoogleMap.js
+++ ecobus/src/components/GoogleMap.js
@@ -1,29 +1,47 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef, useCallback} from 'react'
 import { GoogleMap,
          useJsApiLoader,
          Marker,
          InfoWindow,
          useLoadScript } from '@react-google-maps/api';
 
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+
+import "@reach/combobox/styles.css";
+
+
 const libraries = ["places"];
 
 const mapContainerStyle = {
-    width: '30vw',
+    width: '100vw',
     height: '100vh'
 };
 
 const options = {
     disableDefaultUI: true,
     zoomControl: true,
-
 }
+
+const latVancouver = 49.28;
+const lngVancouver = -123.12;
+const searchRadius = 10000; // meters
 
 function GMap() {
 
-    const [currentLat, setCurrentLat] = useState(49.28);
-    const [currentLng, setCurrentLng] = useState(-123.12);
+    const [currentLat, setCurrentLat] = useState(latVancouver);
+    const [currentLng, setCurrentLng] = useState(lngVancouver);
 
-
+    // Update current location
     useEffect(()=>{
         if("geolocation" in navigator){
             navigator.geolocation.getCurrentPosition(function(position) {
@@ -32,14 +50,26 @@ function GMap() {
                 setCurrentLng(position.coords.longitude);
               });
         } else {
-            console.log("Not Available");
+            console.log("GeoLocation Not Available");
         }
     },[])
 
+    const mapRef = useRef(); // this allows us to retain state w/o re-rendering
+    const onMapLoad = useCallback((map) => {
+        mapRef.current = map;
+    }, []); // 
+
     const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: process.env.GOOGLE_MAP_API_KEY,
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
         libraries
       });
+
+
+    const panTo = useCallback(({lat, lng}) => {
+        mapRef.current.panTo({lat, lng});
+        mapRef.current.setZoom(14);
+
+    },[]);
 
     if (loadError) return "error";
     if (!isLoaded) return "Loading";
@@ -47,13 +77,16 @@ function GMap() {
     return(
         <>
             <div>
+                <Search panTo={panTo} />                
+
                 <GoogleMap 
                 mapContainerStyle={mapContainerStyle} 
-                zoom={8} 
+                zoom={13} 
                 center={{lat: currentLat,
                          lng: currentLng}}
                 options={options}
                 onClick={(event) => {console.log(event)}}
+                onLoad={onMapLoad}
                 >
                     <Marker position={{lat: currentLat,
                          lng: currentLng}} />
@@ -66,5 +99,53 @@ function GMap() {
     );
     };
 
-    
+    //https://www.npmjs.com/package/use-places-autocomplete        
+    function Search( {panTo} ) {
+        const {
+          ready,
+          value,
+          suggestions: { status, data },
+          setValue,
+          clearSuggestions,
+        } = usePlacesAutocomplete({
+          requestOptions: {
+            location: { lat: () => latVancouver, lng: () => lngVancouver },
+            radius: searchRadius,
+          },
+        });
+
+
+        return (
+            <div className="search">
+                <Combobox onSelect={async (address) => {
+                            setValue(address, false);
+                            clearSuggestions();
+
+                            try {
+                                const results = await getGeocode( {address});
+                                const { lat, lng } = await getLatLng(results[0]);
+                                panTo({ lat, lng });
+                            } catch (error) {
+                                console.log("error");
+                            }        
+                            }}
+                    >
+                <ComboboxInput value={value} 
+                    onChange={(e) => {
+                    setValue(e.target.value);
+                    }}
+                    disabled={!ready}
+                    placeholder="Enter Location" 
+                />
+                <ComboboxPopover>
+                    <ComboboxList>
+                        {status === "OK" && data.map(({ id, description}) => (
+                            <ComboboxOption key={id} value={description} />
+                        ))}
+                    </ComboboxList>
+                </ComboboxPopover>
+                </Combobox>
+            </div>
+        )
+    }
 export default GMap
