9ca70876c1370f16b6cd3cd27869664e1e0dfb61
Austin He <ahe8@my.bcit.ca>
2021-05-14 10:35:33 -0700

    fix form so you can now edit

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -3,21 +3,19 @@ import React, { useState } from "react";
 import { useHistory } from "react-router";
 import firebase from "firebase/app";
 import "firebase/auth";
-import "../styles/Profile.css";
-import Toggle from "../images/toggle.png";
+import "../styles/profile.css";
 import Edit from "../images/editbutton.png";
 import logo from "../images/logo.png";
-import Button from 'react-bootstrap/Button';
-import Accordion from 'react-bootstrap/Accordion';
-
+import { Accordion, Button, Card, ListGroup } from 'react-bootstrap';
+import {useForm} from 'react-hook-form';
 
 const Profile = () => {
     const history = useHistory();
     const user = firebase.auth().currentUser;
     // const [toggle, setToggle] = useState(false);
+    // const [name, changeName] = useState("");
 
-    const [name, changeName] = useState("");
-    const [email, changeEmail] = useState("");
+    const { register, handleSubmit, watch, formState: { errors } } = useForm(); //taken from https://react-hook-form.com/get-started
 
     const totalTrips = 0;
     const totalDistance = 0;
@@ -28,15 +26,6 @@ const Profile = () => {
         history.push("/");
     }
 
-    const onToggle = () => {
-        return (
-            <div className="stats">
-                <p>Total Trips: {totalTrips}</p>
-                <p>Total Distance Travelled: {totalDistance}</p>
-                <p>Total Emissions Saved: {totalEmissionSaved}</p>
-            </div>
-        )
-    }
 
     // const onEdit =  async (event) => {
 
@@ -45,25 +34,18 @@ const Profile = () => {
 
 
 
-    const onChange = (event) => {
-        const {
-            target: { name, value }
-        } = event;
-        if (name === "name") {
-            changeName(value);
-        } else if (name === "email") {
-            changeEmail(value);
-        }
+    const onSubmit = (data) => {
+        console.log(data)
     };
 
+
     return (
-        <>
+
+        <div className="profileBody">
             <div id="header">
                 <a href="."><input type="image" src={logo} id="logo" alt="logo" /></a>
-                <Button variant="primary">Hello</Button>
-                <Button variant="secondary">Hello</Button>
-                <Button variant="danger" onClick={onLogoutClick} id="logout">Log Out</Button>
-                <hr/>
+                <Button variant="danger btn-sm" onClick={onLogoutClick} id="logout">Log Out</Button>
+                <hr />
             </div>
 
             <div className="Profile">
@@ -71,32 +53,82 @@ const Profile = () => {
                     <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="Avatar" /> {/*query image later from database*/}
                     <input type="image" id="avataredit" src={Edit} alt="AvatarEdit" />
                 </div>
-                <div>
-                    <input id="FirstName" type="text" placeholder="Name:" name="name" />
-                    <input type="image" id="editbutton" src={Edit} alt="Edit" />
-                </div>
-                <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input id="FirstName" type="text" placeholder="Name" name="name" defaultValue="Austin" {...register("name")}/>
+                    <img src={Edit} id="editbutton" alt="Edit"/>
+                    {/* <input type="image" id="editbutton" src={Edit} alt="Edit" /> */}
+                    <input type="email" placeholder="Email" name="email" defaultValue={user["email"]} {...register("email")} />
+                    <img src={Edit} id="editbutton" alt="Edit"/>
+                    {/* <input type="image" id="editbutton" src={Edit} alt="Edit" /> */}
+                    <br/>
+                    <input type="submit" id="saveEdits" value="Save Changes"/>
+                </form>
 
-                    <input type="email" placeholder="Email" value={user["email"]} name="email" onChange={onChange} />
-                    <input type="image" id="editbutton" src={Edit} alt="Edit" />
-                </div>
+                <div id="userHistory">
+                    <Accordion>
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                    User Statistics
+                                </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item variant="secondary">Total Trips: {totalTrips}</ListGroup.Item>
+                                    <ListGroup.Item variant="secondary">Total Distance Travelled: {totalDistance}</ListGroup.Item>
+                                    <ListGroup.Item variant="secondary">Total Emissions Saved: {totalEmissionSaved}</ListGroup.Item>
+                                </ListGroup>
+                            </Accordion.Collapse>
+                        </Card>
 
-                <div id="userStats">
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                                    Route History
+                                </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="1">
+                                <ListGroup variant="flush">
+                                    <Accordion>
+                                        <Card>
+                                            <Card.Header>
+                                                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                                    Route 1
+                                    </Accordion.Toggle>
+                                            </Card.Header>
+                                            <Accordion.Collapse eventKey="0">
+                                                <ListGroup variant="flush">
+                                                    <ListGroup.Item variant="secondary">Starting location: UBC</ListGroup.Item>
+                                                    <ListGroup.Item variant="secondary">Ending location: Metrotown</ListGroup.Item>
+                                                    <ListGroup.Item variant="secondary">Total emissions saved: 7.5kg of CO2</ListGroup.Item>
+                                                </ListGroup>
+                                            </Accordion.Collapse>
+                                        </Card>
 
-                    <input type="image" id="togglebutton" src={Toggle} alt="Toggle" onClick={onToggle} />
-                    <p>User Statistics</p>
-
-                </div>
-
-                <div id="routeHistory">
-                    <input type="image" id="togglebutton" src={Toggle} alt="Toggle" onClick={onToggle} />
-                    <p>Route History</p>
-
+                                        <Card>
+                                            <Card.Header>
+                                                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                                                    Route 2
+                                        </Accordion.Toggle>
+                                            </Card.Header>
+                                            <Accordion.Collapse eventKey="1">
+                                                <ListGroup variant="flush">
+                                                    <ListGroup.Item variant="secondary">Starting location: BCIT Downtown</ListGroup.Item>
+                                                    <ListGroup.Item variant="secondary">Ending location: BCIT Burnaby</ListGroup.Item>
+                                                    <ListGroup.Item variant="secondary">Total emissions saved: 6kg of CO2</ListGroup.Item>
+                                                </ListGroup>
+                                            </Accordion.Collapse>
+                                        </Card>
+                                    </Accordion>
+                                </ListGroup>
+                            </Accordion.Collapse>
+                        </Card>
+                    </Accordion>
                 </div>
 
             </div>
-
-        </>);
+        </div>
+    );
 }
 
 
