0688ca3fdfd35bcca7b5e1758a33cabefbbe03ab
CALEB VERMA <halasmish@gmail.com>
2021-05-14 02:03:23 -0700

    add chris's about us blurb

--- src/routes/About_Us.js
+++ src/routes/About_Us.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import '../styles/about_us.css';
-import Caleb from '../images/caleb.png'
+import Caleb from '../images/Caleb.png'
 import Dylan from 'images/Dylan.png'
 import Austin from 'images/Austin.png'
 import Chris from 'images/Chris.jpeg'
@@ -23,15 +23,19 @@ const About_Us = () => <main>
         <h2 id="Chris"><a href='https://github.com/Christopher-Eunho'>Eunho "Chris" Jung</a></h2>
         <img class="PhotoID" src={Chris} alt="Picture of Chris"/>
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            An aspiring programmer and the Product Owner of EcoBus. While working in the semiconductor industry of South Korea as a mechanical engineer, he was fascinated by 
+            how much the world was being transformed by technology  So, he decided to transform himself from a hardware engineer to a software engineer and is now studying 
+            programming in Vancouver. As the Product Owner of EcoBus, he convieved of the application's features and managed the overall development of the application. He also
+            worked with the Google Maps API to implement the core feature of mapping a user's route.
         </p>
     </section>
     <section>
         <h2 id="Caleb"><a href='https://github.com/fijiman24'>Caleb Verma</a></h2>
         <img class="PhotoID" src={Caleb} alt="Picture of Caleb" />
         <p>
-            Caleb is an aspiring software developer. He graduated from UBC in 2020 with a major in Philosophy and, that very same year, decided to apply to BCIT's CST program on a whim.
-            He is fascinated by the seemingly limitless potential of programming; whether it's to create video games or robots that can explore Mars, programming offers a way to solve any problem.   
+            Caleb is an aspiring software developer. He graduated from UBC in 2020 with a major in Philosophy and, that very same year, decided to apply to BCIT's CST 
+            program on a whim. He is fascinated by the seemingly limitless potential of programming; whether it's to create video games or robots that can explore Mars, 
+            programming offers a way to solve any problem.   
         </p>
     </section>
     <section>
@@ -39,15 +43,17 @@ const About_Us = () => <main>
         <img class="PhotoID" src={Austin} alt="Picture of Austin"/>
         <p>
             Austin is a a recent graduate of Capilano University where he got his Bachelor of Business Administration degree with a concentration in Accounting.
-            He joined the CST program to work on projects and learn how to automate tasks so he can enjoy the simple things in life. Austin is excited to see what new technologies will be developed in the future to combat the global climate crisis.
+            He joined the CST program to work on projects and learn how to automate tasks so he can enjoy the simple things in life. Austin is excited to see what 
+            new technologies will be developed in the future to combat the global climate crisis.
         </p>
     </section>
     <section>
         <h2 id="Dylan"><a href='https://github.com/CDGCodes'>Dylan Punter</a></h2>
         <img class="PhotoID" src={Dylan} alt="Picture of Dylan"/>
         <p>
-            Dylan is a student who recently graduated from high school. After taking a semester off to save money, he applied to CIT, before changing his mind, and applying to CST the following term.
-            He has always been interested in the world of computers, rebuilding his own countless times, and originally joined CST with the intent of becoming a game developer. Now, he wants to use his programming skills to benefit the world however he can.
+            Dylan is a student who recently graduated from high school. After taking a semester off to save money, he applied to CIT, before changing his mind, 
+            and applying to CST the following term. He has always been interested in the world of computers, rebuilding his own countless times, and originally 
+            joined CST with the intent of becoming a game developer. Now, he wants to use his programming skills to benefit the world however he can.
         </p>
     </section>
 </main>;
